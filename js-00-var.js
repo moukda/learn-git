c4aee0dbf75ecd48743423eae7bf1235b1c9c9d7
moukda <moukda.ks@gmail.com>
2022-10-24 16:30:49 +0700

    my first commit

---
+++ js-00-var.js
@@ -0,0 +1,5 @@
+let a number;
+
+
+let a =10; //ຮູບແບບໂຕແປ
+let a
---
+++ jv-03-condition.js
@@ -0,0 +1,24 @@
+/* 
+    Comparison: ==, ===, !=, !==, >, <, >=, <=
+*/
+
+var money = 150;
+
+    if (money >= 150){
+        console.log('buy');
+    } else{
+        console.log('noy buy');
+    }
+
+var num = 99;
+    if(num <= 20){
+        console.log("E");
+    } else if(num <=30){
+        console.log("D");
+    } else if(num <=50){
+        console.log("C");
+    } else if(num <=80){
+        console.log("B");
+    } else if(num <=100){
+        console.log("A");
+    }
